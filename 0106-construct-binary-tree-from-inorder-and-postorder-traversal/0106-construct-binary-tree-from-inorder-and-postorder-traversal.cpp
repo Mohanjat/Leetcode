3e3ee7657df82f1a6db6c3624eb43edbcd2a62cb
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-02-08 01:25:52 +0530

    Time: 15 ms (54.62%), Space: 26.4 MB (57.48%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -21,7 +21,7 @@ public:
     }
     
     TreeNode* newTree(vector<int>& inorder, vector<int>& postorder,int size, int &postIndex,int start
-                      ,int end){
+                      ,int end,unordered_map<int,int> &map){
         
         //base case
         if(start>end || postIndex < 0){
@@ -31,12 +31,18 @@ public:
         int element = postorder[postIndex--];
         TreeNode* root = new TreeNode(element);
         
-        int pos = findPos(inorder,element,size);
+        // int pos = findPos(inorder,element,size);
+        
+        int pos = map[element];
+        
+        // if(map.find(map[element]) != map.end()){
+        //     pos = map[element];
+        // }
         
         //first we have to nodes in the right part of root because the order of the postOrder 
         //Traversal is Left Right Node that why 
-        root->right = newTree(inorder,postorder,size,postIndex,pos+1,end);
-        root->left = newTree(inorder,postorder,size,postIndex,start,pos-1);
+        root->right = newTree(inorder,postorder,size,postIndex,pos+1,end,map);
+        root->left = newTree(inorder,postorder,size,postIndex,start,pos-1,map);
         
         return root;
     }
@@ -48,7 +54,12 @@ public:
         int start = 0;
         int end = size-1;
         
-        return newTree(inorder,postorder,size,postIndex,start,end);
+        unordered_map<int,int>map;
+        
+        for(int i=0; i<size; i++){
+            map[inorder[i]] = i;
+        }
+        return newTree(inorder,postorder,size,postIndex,start,end,map);
         
     }
 };
