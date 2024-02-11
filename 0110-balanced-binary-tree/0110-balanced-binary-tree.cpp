80b504940036956184858aeec4aec11d9eaa56aa
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-02-12 00:41:51 +0530

    Time: 12 ms (43.65%), Space: 21.5 MB (37.96%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -21,29 +21,45 @@ public:
         return max(lefth, righth) +1;
     }
     
-    
+    int height(TreeNode* root, bool &ans){
+        
+        if(!root) return 0;
+        
+       int lh = height(root->left,ans);
+       int rh = height(root->right,ans);
+       
+        if(abs(lh-rh) > 1) ans = false;
+        
+        return max(lh,rh)+1;
+    }
     bool isBalanced(TreeNode* root) {
         
-        //base case 
-        if(root == NULL){
-            //agr hum last tk phuch gye h aur false return nhi h iska mtlb tree balanced ho skta h
-            return true;
-        }
+//         //base case 
+//         if(root == NULL){
+//             //agr hum last tk phuch gye h aur false return nhi h iska mtlb tree balanced ho skta h
+//             return true;
+//         }
         
-        //let calulate the left part height 
-        int leftH = leftHeight(root->left);
-        cout<<"height of left part "<<leftH<<endl;
-        int rightH = leftHeight(root->right);
-        cout<<"height of right part "<<rightH<<endl;
+//         //let calulate the left part height 
+//         int leftH = leftHeight(root->left);
+//         cout<<"height of left part "<<leftH<<endl;
+//         int rightH = leftHeight(root->right);
+//         cout<<"height of right part "<<rightH<<endl;
         
-        if(abs(leftH-rightH) > 1){
-            return false;
-        }
+//         if(abs(leftH-rightH) > 1){
+//             return false;
+//         }
         
-        bool left = isBalanced(root->left);
-        bool right = isBalanced(root->right);
+//         bool left = isBalanced(root->left);
+//         bool right = isBalanced(root->right);
         
-        return left & right;
+//         return left & right;
+        
+        bool ans = true;
+        
+        height(root,ans);
+        
+        return ans;
         
     }
 };
