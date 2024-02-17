d9c745cd183e572cd53aa3408cd50bde353235fb
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-02-18 03:18:16 +0530

    Time: 19 ms (56.87%), Space: 17.6 MB (48.23%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -11,42 +11,56 @@
  */
 class Solution {
 public:
-    
-    void findSum(TreeNode* root,long &targetSum,long &temp,long& sum){
+//     void findSum(TreeNode* root,long &targetSum,long &temp,long& sum){
         
-        if(root == NULL) return ;
+//         if(root == NULL) return ;
         
-        //phle isliye add ki h value kyo leaf node ke case ke ander 
-        //equal condition check kiye bina hi return ho jaa rhy thy
-        sum += root->val;
+//         //phle isliye add ki h value kyo leaf node ke case ke ander 
+//         //equal condition check kiye bina hi return ho jaa rhy thy
+//         //yha maine glti kri thi bhai remember
+//         sum += root->val;
         
-        if(targetSum == sum) temp++;
+//         if(targetSum == sum) temp++;
         
       
-        findSum(root->left,targetSum,temp,sum);
+//         findSum(root->left,targetSum,temp,sum);
         
-        findSum(root->right,targetSum,temp,sum);
+//         findSum(root->right,targetSum,temp,sum);
         
-        sum -= root->val;
-    }
+//         sum -= root->val;
+//     }
     
-    void calSum(TreeNode* root, long &targetSum, long&ans){
+//     void calSum(TreeNode* root, long &targetSum, long&ans){
+//         if(root == NULL) return;
+        
+//         //first of all try to calclue sum including curr node
+//         long temp = 0;
+//         long sum = 0;
+        
+//         findSum(root,targetSum,temp,sum);
+        
+//         // if(temp == 0) cout<<root->val<<" ";
+
+//         ans += temp;
+        
+//         //answer left m bhi check kr lo
+//         calSum(root->left,targetSum,ans);
+//         //right m bhi check kr lo bhai
+//         calSum(root->right,targetSum,ans);
+        
+//     }
+    
+    int ans = 0;
+    void findSum(TreeNode* root, long targetSum,int &ans){
+        
         if(root == NULL) return;
         
-        //first of all try to calclue sum including curr node
-        long temp = 0;
-        long sum = 0;
+        if(targetSum == root->val){
+            ans++;
+        }
         
-        findSum(root,targetSum,temp,sum);
-        
-        // if(temp == 0) cout<<root->val<<" ";
-
-        ans += temp;
-        
-        //answer left m bhi check kr lo
-        calSum(root->left,targetSum,ans);
-        //right m bhi check kr lo bhai
-        calSum(root->right,targetSum,ans);
+        findSum(root->left,targetSum-root->val,ans);
+        findSum(root->right,targetSum-root->val,ans);
         
     }
     
@@ -56,11 +70,23 @@ public:
         //ya phir right se ayega 
         //ya phir curr node ko include krke aayega
         
-        long ans = 0;
-        
-        calSum(root,targetSum,ans);
+        //har baar new ans ban rha h so its a bug
+        // int ans = 0;
+        if(root){
+            findSum(root,targetSum,ans);
+            
+            pathSum(root->left,targetSum);
+            pathSum(root->right,targetSum);
+            
+        }
         
         return ans;
+        
+//         long ans = 0;
+        
+//         calSum(root,targetSum,ans);
+        
+//         return ans;
         
     }
 };
