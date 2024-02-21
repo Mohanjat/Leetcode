1b0a49f5c65003d549617c6b16838eac6e1ba054
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-02-22 01:15:25 +0530

    Time: 27 ms (37.97%), Space: 21.8 MB (81.77%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -12,29 +12,47 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         
+        //approach ## 2 -- solve using BST property
+        
         if(root == NULL) return NULL;
         
-        if(root == p) return p;
-        if(root == q) return q;
-        
-        //left se answer lekr aao 
-        TreeNode* left = lowestCommonAncestor(root->left,p,q);
-        //right se answer lekr aao
-        TreeNode* right = lowestCommonAncestor(root->right,p,q);
-        
-        
-        if(left == NULL && right == NULL){
-            return NULL;
+        //agr dono values p and q choti h root ki value se to left m search kr lo
+        if(root->val > p->val && root->val > q->val){
+            return lowestCommonAncestor(root->left,p,q);
         }
-        else if(left == NULL && right != NULL){
-            return right;
+        else if(root->val < p->val && root->val < q->val){
+            //agr dono values root ki value se bdi h to right m chle jao
+            return lowestCommonAncestor(root->right,p,q);
         }
-        else if(left != NULL && right == NULL){
-            return left;
-        }
-        else{
+        else {
             return root;
         }
+        
+        
+        
+//         if(root == NULL) return NULL;
+        
+//         if(root == p) return p;
+//         if(root == q) return q;
+        
+//         //left se answer lekr aao 
+//         TreeNode* left = lowestCommonAncestor(root->left,p,q);
+//         //right se answer lekr aao
+//         TreeNode* right = lowestCommonAncestor(root->right,p,q);
+        
+        
+//         if(left == NULL && right == NULL){
+//             return NULL;
+//         }
+//         else if(left == NULL && right != NULL){
+//             return right;
+//         }
+//         else if(left != NULL && right == NULL){
+//             return left;
+//         }
+//         else{
+//             return root;
+//         }
         
     }
 };
