cf2050badd96126105e9b7b7d139d1dc10bc2385
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-03-06 01:37:06 +0530

    Time: 92 ms (5.10%), Space: 13.8 MB (81.29%) - LeetHub

---
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        
+        int len = s.length();
+        int i=0; 
+        int j=len-1;
+      
+        while(i<j && s.length()>2){
+            //first of all check suffix and prefix
+            
+            if(s[i] == s[j]){
+                //now apply the algo
+                //step1-- cal the prefix
+                while(i<j){
+                    if(s[i] != s[i+1]){
+                        i++;
+                        break;
+                    }else if(i+1 != j && s[i] == s[i+1]){
+                       i++;                
+                    }else{
+                        break;
+                    }
+        
+                }
+                
+                //step2-- cal the suffix
+                while(i<j){
+                    if(s[j] != s[j-1]){
+                        j--;
+                        break;
+                    }else if(j-1 != i && s[j] == s[j-1]){
+                        j--;
+                    }
+                    else{
+                        break;
+                    }
+                }
+                
+                //step5-- deleting prefix and suffix
+        
+                int l = s.length()-j-1;
+                s.erase(s.begin(),s.begin()+i);
+                s.erase(s.end()-l,s.end());
+         
+            }
+            else{
+                break;
+            }
+            i = 0;
+            j = s.length()-1;
+            
+        }
+        
+        // if(s.length() == 1) return 0;
+        if(s.length() == 2){
+            if(s[0] == s[1]) return 0;
+        }
+        
+        return s.length();
+    }
+};
