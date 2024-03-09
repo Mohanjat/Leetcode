999455d55c076e8eadf9ae4d8f59aad344b10203
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-03-10 01:05:56 +0530

    Time: 68 ms (68.97%), Space: 52.8 MB (74.59%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-       
-        set<int> s;
         
-        for(int i=0; i<nums1.size(); i++){
-            s.insert(nums1[i]);
-        }
-        
+        int i=0;
+        int j=0;
         int ans = -1;
         
-        for(int i=0; i<nums2.size(); i++){
-            if(s.find(nums2[i]) != s.end()){
-                ans = nums2[i];
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i] == nums2[j]){
+                ans = nums1[i];
                 break;
             }
+            
+            if(nums1[i] < nums2[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
         }
-     return ans;  
+        return ans;
+        
+       
+//         set<int> s;
+        
+//         for(int i=0; i<nums1.size(); i++){
+//             s.insert(nums1[i]);
+//         }
+        
+//         int ans = -1;
+        
+//         for(int i=0; i<nums2.size(); i++){
+//             if(s.find(nums2[i]) != s.end()){
+//                 ans = nums2[i];
+//                 break;
+//             }
+//         }
+//      return ans;  
     }
 };
