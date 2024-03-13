541531efadbdf252f1e162121cc94323f6caeb91
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-03-14 01:06:46 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (15.62%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -2,32 +2,45 @@ class Solution {
 public:
     int pivotInteger(int n) {
         
-        int pivot = n/2;
+        int sum = n*(n+1)/2;
         
-        int leftSum = 0;
-        int rightSum = 0;
+        double a = sqrt(sum);
         
-        for(int i=1; i<=pivot; i++){
-            leftSum += i;
+        if(a - ceil(a) == 0){
+            return int(a);
+        }
+        else{
+            return -1;
         }
         
-        for(int i=pivot; i<=n; i++){
-            rightSum += i;
-        }
         
-        while(leftSum <= rightSum && pivot<=n){
+        
+//         int pivot = n/2;
+        
+//         int leftSum = 0;
+//         int rightSum = 0;
+        
+//         for(int i=1; i<=pivot; i++){
+//             leftSum += i;
+//         }
+        
+//         for(int i=pivot; i<=n; i++){
+//             rightSum += i;
+//         }
+        
+//         while(leftSum <= rightSum && pivot<=n){
             
-            if(rightSum == leftSum) return pivot;
+//             if(rightSum == leftSum) return pivot;
             
-            if(leftSum < rightSum){
-                int temp = pivot;
-                pivot++;
+//             if(leftSum < rightSum){
+//                 int temp = pivot;
+//                 pivot++;
                 
-                leftSum += pivot;
-                rightSum -= temp;
-            }
+//                 leftSum += pivot;
+//                 rightSum -= temp;
+//             }
             
-        }
+//         }
         
         return -1;
     }
