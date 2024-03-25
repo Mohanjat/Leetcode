cb89592a1adab13a6dc0f55964fe1d54dbce65d5
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-03-25 20:44:53 +0530

    Time: 31 ms (94.91%), Space: 36 MB (77.98%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -2,19 +2,32 @@ class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         
-        unordered_map<int,int> map;
+          vector<int> ans;
         
-        for(auto i:nums){
-            map[i]++;
-        }
-        
-        vector<int> ans;
-        
-        for(auto i:map){
-            if(i.second>1){
-                ans.push_back(i.first);
+          for (int i = 0; i < nums.size(); ++i) {
+            int index = abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                ans.push_back(index + 1);
+            } else {
+                nums[index] *= -1;
             }
         }
+        
         return ans;
+        
+//         unordered_map<int,int> map;
+        
+//         for(auto i:nums){
+//             map[i]++;
+//         }
+        
+//         vector<int> ans;
+        
+//         for(auto i:map){
+//             if(i.second>1){
+//                 ans.push_back(i.first);
+//             }
+//         }
+//         return ans;
     }
 };
