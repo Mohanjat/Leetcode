cb48e2d2aa96dd82bfa0e43a0287fd384d2d6d2e
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-04-09 20:36:05 +0530

    Time: 4 ms (41.19%), Space: 9.3 MB (91.31%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -2,22 +2,47 @@ class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         
-        int sec = 0;
+        //optimal method
         
-        while(true){
-            bool check = false;
-            for(int i=0; i<tickets.size(); i++){
-                if(tickets[i] == 0) continue;
-                tickets[i] -= 1;
-                sec++;
-                if(i == k && tickets[i] == 0){
-                    check = true;
-                    return sec;
-                }
+        int sec = tickets[k];
+        
+        for(int i=0; i<k; i++){            
+            if(tickets[i] >= tickets[k]){
+                sec += tickets[k];
+            }
+            else{
+                sec += tickets[i];
             }
         }
         
-        return 0;
+        for(int i=k+1; i<tickets.size(); i++){
+            if(tickets[i] >= tickets[k]){
+                sec += tickets[k]-1;
+            }
+            else{
+                sec += tickets[i];
+            }
+        }
+        
+        return sec;
+        
+        
+//         int sec = 0;
+        
+//         while(true){
+//             bool check = false;
+//             for(int i=0; i<tickets.size(); i++){
+//                 if(tickets[i] == 0) continue;
+//                 tickets[i] -= 1;
+//                 sec++;
+//                 if(i == k && tickets[i] == 0){
+//                     check = true;
+//                     return sec;
+//                 }
+//             }
+//         }
+        
+//         return 0;
         
     }
 };
