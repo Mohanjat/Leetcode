460d7cbe83b8780f5018e1eee76fd9e01b594303
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-04-25 20:43:49 +0530

    Time: 8 ms (79.45%), Space: 15.7 MB (7.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,13 +2,30 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
+        //using hash-table
+        unordered_map<int,int> hashMap;
+        
         for(int i=0; i<nums.size(); i++){
-            for(int j=i+1; j<nums.size(); j++){
-                if(target-nums[i]-nums[j] == 0){
-                    return {i,j};
-                }
+            hashMap[nums[i]] = i;
+        }
+        
+        for(int i=0; i<nums.size(); i++){
+            int temp = target-nums[i];
+            if(hashMap.find(temp) != hashMap.end() &&hashMap[temp] != i){
+                return {i,hashMap[temp]};
             }
         }
-        return {0,0};
+        
+        return {};
+        //brute force
+        
+        // for(int i=0; i<nums.size(); i++){
+        //     for(int j=i+1; j<nums.size(); j++){
+        //         if(target-nums[i]-nums[j] == 0){
+        //             return {i,j};
+        //         }
+        //     }
+        // }
+        // return {0,0};
     }
 };
