7e005ffaee4ec1c0be3ba60086331b71e73531ca
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-04-27 01:01:59 +0530

    Time: 84 ms (26.99%), Space: 70.2 MB (29.26%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,67 +1,21 @@
 class Solution {
 public:
-
-    int maxSumArr(vector<int> &nums, int s, int e){
-
-        if(s >= e) return nums[s];
-
-        int mid = s + ((e-s) >> 1);
-
-        //left ka ans nikal lo
-        int leftMax = maxSumArr(nums, s, mid);
-        //right ka ans nikal lo
-        int rightMax = maxSumArr(nums, mid+1, e);
-
-        int leftBorderSum = 0, rightBorderSum = 0;
-        int maxleftBorderSum = INT_MIN, maxrightBorderSum = INT_MIN;
-
-        //left part ko traverse kr lo
-        for(int i=mid; i>=s; i--){
-            leftBorderSum += nums[i];
-            if(leftBorderSum > maxleftBorderSum) maxleftBorderSum = leftBorderSum;
-        }
-
-        //right part ko traverse kr lo
-        for(int i=mid+1; i<=e; i++){
-            rightBorderSum += nums[i];
-            if(rightBorderSum > maxrightBorderSum) maxrightBorderSum = rightBorderSum;
-        }
-
-        int crossBorderSum = maxrightBorderSum + maxleftBorderSum;
-
-        return max(leftMax,max(rightMax,crossBorderSum));
-        
-    }
-
     int maxSubArray(vector<int>& nums) {
-
-        //By Merge Sort
-
-        return maxSumArr(nums,0,nums.size()-1);
-
-
-
-
-
-
-
-
-        // //By Kadane's Algo
-
-        // int sum = 0;
-        // int maxi = INT_MIN;
-
-        // for(int i=0; i<nums.size(); i++){
-        //     //step1:- current sum 
-        //     sum += nums[i];
-
-        //     //step2:- max sum ko update kr lo
-        //     maxi = max(maxi,sum);
-
-        //     //step3:- agr sum -ve ho jaaye to sum ko zero kr do
-        //     if(sum < 0) sum = 0;
-        // }
+        //kadane's algo
+        int maxSum = INT_MIN;
+        int sum = 0;
         
-        // return maxi;
+        for(int i=0; i<nums.size(); i++){
+            //add the curr element
+            sum += nums[i];
+            
+            //update the max sum 
+            maxSum = max(maxSum,sum);
+            
+            //check kr lo khi sum negative m to nhi h
+            if(sum < 0) sum = 0;
+        }
+         
+        return maxSum;
     }
 };
