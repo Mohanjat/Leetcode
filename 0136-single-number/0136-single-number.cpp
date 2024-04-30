88ae9a4b115ebe5cd7dd7f1110f4f7d2a326ea94
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-05-01 01:14:19 +0530

    Time: 7 ms (96.53%), Space: 19.3 MB (32.29%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int cum_xor = 0;
+        for(int i=0; i<nums.size(); i++){
+            cum_xor ^= nums[i];
+        }
+        return cum_xor;
+    }
+};
