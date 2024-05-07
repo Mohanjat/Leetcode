b0200d6927bfadd82759991bf5260cfe465431c9
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-05-08 01:29:31 +0530

    Time: 186 ms (40.54%), Space: 120.6 MB (97.03%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
@@ -12,6 +12,8 @@ class Solution {
 public:
     
     ListNode* solve(ListNode*temp,int&digit){
+        
+        
         if(!temp) return nullptr;
         
         ListNode* nextNode = solve(temp->next,digit);
@@ -30,35 +32,66 @@ public:
     
     ListNode* doubleIt(ListNode* head){
         
-        //Approach#2 traverse only Left to Right using two pointers
-    
-        ListNode* curr = head;
-        ListNode* prev = nullptr;
+        //Approach#3 left to right using one pointer
         
-        while(curr != NULL){
-            int value = (curr->val)*2;
-            
-            if(value<10){ //normal case where value is 1-9 direct add this 
-                curr->val = value;
-            }
-            //add extra carry to the previous node
-            else if(prev == NULL){//we are at first node and there is carry {
-                ListNode* newNode = new ListNode(1);
-                newNode->next = curr;
-                head = newNode;
-                //make sure to update the curr value with the right most digit of value
-                curr->val = value%10;
-            }
-            else{
-                curr->val = value%10;
-                //if value is greater then 10, then carry will definetly 1
-                prev->val += 1;
-            }
-            prev = curr;
-            curr = curr->next;
+        
+        //intitution is that carry sirf tb hi aayega tb node->val >= 5 hogi 
+        //to bhai phle hi check kr lo na ki curr se next waali node ki value greater than 5 se bhdi to nhi agr h to phle hi ek carry curr node m add kr do simple is that
+        
+        //one corner case is that agr phli hi node ki value 5 ya 5 se bdi hui to 
+        //handle seperately
+        
+        ListNode* curr = head;
+        
+        if(head->val>=5){
+            ListNode* firstNode = new ListNode(1);
+            firstNode->next = head;
+            head = firstNode;
         }
         
+        while(curr != NULL){
+            curr->val = (curr->val*2)%10;
+            
+            if(curr->next != nullptr && curr->next->val >= 5){
+                //mtlb this is kind of absolute prediction that
+                //pkka carry aane waala h to usko phle hi add kr do
+                curr->val += 1;
+            }
+            curr = curr->next;
+        }
         return head;
+        
+        
+        
+        //Approach#2 traverse only Left to Right using two pointers
+    
+//         ListNode* curr = head;
+//         ListNode* prev = nullptr;
+        
+//         while(curr != NULL){
+//             int value = (curr->val)*2;
+            
+//             if(value<10){ //normal case where value is 1-9 direct add this 
+//                 curr->val = value;
+//             }
+//             //add extra carry to the previous node
+//             else if(prev == NULL){//we are at first node and there is carry {
+//                 ListNode* newNode = new ListNode(1);
+//                 newNode->next = curr;
+//                 head = newNode;
+//                 //make sure to update the curr value with the right most digit of value
+//                 curr->val = value%10;
+//             }
+//             else{
+//                 curr->val = value%10;
+//                 //if value is greater then 10, then carry will definetly 1
+//                 prev->val += 1;
+//             }
+//             prev = curr;
+//             curr = curr->next;
+//         }
+        
+//         return head;
         
         
         //Approach#1 using Recursion
