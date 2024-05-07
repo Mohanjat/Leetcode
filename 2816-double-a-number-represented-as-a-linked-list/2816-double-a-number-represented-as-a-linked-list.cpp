9cfe64237a8b49a6f6ea5e2ed9acbac9d7b272b5
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-05-08 01:14:38 +0530

    Time: 180 ms (49.15%), Space: 120.5 MB (97.03%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
@@ -29,21 +29,54 @@ public:
     }
     
     ListNode* doubleIt(ListNode* head){
-        int digit = 0;
-        ListNode*temp = head;
-        head = solve(temp,digit);
         
-        while(digit>0){
-            int rightDigit = digit%10;
-            //create a new node of this
-            ListNode* newNode = new ListNode(rightDigit);
+        //Approach#2 traverse only Left to Right using two pointers
+    
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+        
+        while(curr != NULL){
+            int value = (curr->val)*2;
             
-            newNode->next = head;
-            head = newNode;
-            digit = digit/10;
+            if(value<10){ //normal case where value is 1-9 direct add this 
+                curr->val = value;
+            }
+            //add extra carry to the previous node
+            else if(prev == NULL){//we are at first node and there is carry {
+                ListNode* newNode = new ListNode(1);
+                newNode->next = curr;
+                head = newNode;
+                //make sure to update the curr value with the right most digit of value
+                curr->val = value%10;
+            }
+            else{
+                curr->val = value%10;
+                //if value is greater then 10, then carry will definetly 1
+                prev->val += 1;
+            }
+            prev = curr;
+            curr = curr->next;
         }
         
         return head;
         
-    }
+        
+        //Approach#1 using Recursion
+        
+//         int digit = 0;
+//         ListNode*temp = head;
+//         head = solve(temp,digit);
+        
+//         while(digit>0){
+//             int rightDigit = digit%10;
+//             //create a new node of this
+//             ListNode* newNode = new ListNode(rightDigit);
+            
+//             newNode->next = head;
+//             head = newNode;
+//             digit = digit/10;
+//         }
+        
+//         return head;
+    }      
 };
