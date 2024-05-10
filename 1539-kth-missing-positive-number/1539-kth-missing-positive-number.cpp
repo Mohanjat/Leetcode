2bf466545546fc9460ad938a8ec534ce16a10d2b
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-05-11 03:04:04 +0530

    Time: 0 ms (100.00%), Space: 12 MB (78.84%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -2,35 +2,56 @@ class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
         
-        int cnt = 0;
-        int firstEle = arr[0];
-        int element = 1;
-        while(element<firstEle){
-            cnt++;
-            if(cnt == k){
-                return element;
+        //Approach#2 Binary Search
+        
+        int s = 0;
+        int e = arr.size()-1;
+        
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            int missing = arr[mid]-(mid+1);
+            if(missing<k){
+               //search in the right part
+                s = mid+1;
             }
-            element++;
-        }
-        
-        for(int i=1; i<arr.size(); i++){
-            int ele = arr[i-1]+1;
-            while(ele < arr[i]){
-                cnt++;
-                if(cnt == k) return ele;
-                ele++;
+            else{
+                e = mid-1;
             }
-        
         }
         
-        //if cnt == 0 means array is having all the elements
-        int newEle = arr[arr.size()-1]+1;
-        while(true){
-            cnt++;
-            if(cnt == k) return newEle;
-            newEle++;
-        }
+        return k+e+1; //return k+s;
         
-        return 0;
+        
+        
+//         int cnt = 0;
+//         int firstEle = arr[0];
+//         int element = 1;
+//         while(element<firstEle){
+//             cnt++;
+//             if(cnt == k){
+//                 return element;
+//             }
+//             element++;
+//         }
+        
+//         for(int i=1; i<arr.size(); i++){
+//             int ele = arr[i-1]+1;
+//             while(ele < arr[i]){
+//                 cnt++;
+//                 if(cnt == k) return ele;
+//                 ele++;
+//             }
+        
+//         }
+        
+//         //if cnt == 0 means array is having all the elements
+//         int newEle = arr[arr.size()-1]+1;
+//         while(true){
+//             cnt++;
+//             if(cnt == k) return newEle;
+//             newEle++;
+//         }
+        
+//         return 0;
     }
 };
