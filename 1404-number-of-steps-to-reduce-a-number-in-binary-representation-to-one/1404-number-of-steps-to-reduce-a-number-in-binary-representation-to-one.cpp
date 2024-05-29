40bcac9a3bb8e4fbaee5747236222206ddaa351e
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-05-30 03:35:58 +0530

    Time: 3 ms (51.78%), Space: 7.6 MB (47.46%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,37 +1,59 @@
 class Solution {
 public:
-    int solve(string s,int &ans){
-       if(s.length() == 1) return ans;
-       ans++;
+//     int solve(string s,int &ans){
+//        if(s.length() == 1) return ans;
+//        ans++;
         
-       if(s[s.length()-1] == '0'){
-           return solve(s.substr(0,s.length()-1),ans);
-       }
+//        if(s[s.length()-1] == '0'){
+//            return solve(s.substr(0,s.length()-1),ans);
+//        }
         
-       int i = s.length()-1;
+//        int i = s.length()-1;
        
-        while(i>=0){
-            if(s[i] == '0'){
-                s[i] = '1';
-                break;
-            }
-            s[i] = '0';
-            i--;
-        }
+//         while(i>=0){
+//             if(s[i] == '0'){
+//                 s[i] = '1';
+//                 break;
+//             }
+//             s[i] = '0';
+//             i--;
+//         }
         
-        if(i == -1){
-            //need to add one char in the string 
-            s.insert(0,1,'1');
-        }
+//         if(i == -1){
+//             //need to add one char in the string 
+//             s.insert(0,1,'1');
+//         }
         
-        return solve(s,ans);
+//         return solve(s,ans);
        
-    }
+//     }
     
     int numSteps(string s){
-        int ans = 0;
         
-        return solve(s,ans);
+        //optimal approach with o(n)
+        
+        int op = 0;
+        int carry = 0;
+        
+        for(int i=s.length()-1; i>=1; i--){
+            if(((s[i]-'0') + carry) % 2 == 1){
+                //odd waala case
+                op += 2;
+                carry = 1;
+            }
+            else{
+               op++;   
+            }
+        }
+        
+        
+        return op+carry;
+        
+        
+        
+//         int ans = 0;
+        
+//         return solve(s,ans);
         
         // return ans;
     }
