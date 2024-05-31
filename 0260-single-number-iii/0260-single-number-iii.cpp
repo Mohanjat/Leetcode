2ff0c799bbbb4c82e0e0e7466fb92c52aadc98ce
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-06-01 03:50:13 +0530

    Time: 3 ms (96.60%), Space: 12.2 MB (87.19%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -2,23 +2,56 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums){
         
-         unordered_map<int,int>mp;
+        //Optimal approach using Bit Manipulation
         
-         for(int i=0; i<nums.size(); i++){
-             if(mp.find(nums[i]) != mp.end()){
-                 mp[nums[i]]--;
-                 if(mp[nums[i]] == 0) mp.erase(nums[i]);
-             }
-             else{
-                 mp[nums[i]]++;
-             }
-         }
+          //step1 == xor all number  = xor
+         //step2 == create a mask using xor & (-xor);
+         //step3 == saperate all elements into two groups using mask & nums[i] 
         
-        vector<int> ans;
+        long long Xor = 0;
         
-        for(auto it:mp){
-            ans.push_back(it.first);
+        for(auto i:nums){
+            Xor ^= i;    
         }
-        return ans;
+        
+        int mask = (Xor) & (-Xor); //xor ki right most set bit ka mask create ho jaayega
+        
+        int group_a = 0;
+        int group_b = 0;
+        
+        //seperate all the elements into the two segments 
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] & mask){
+                //means set bit
+                group_a ^= nums[i];
+            }
+            else{
+                group_b ^= nums[i];
+            }
+        }
+        
+        return {group_a,group_b};
+        
+        
+        
+        
+//          unordered_map<int,int>mp;
+        
+//          for(int i=0; i<nums.size(); i++){
+//              if(mp.find(nums[i]) != mp.end()){
+//                  mp[nums[i]]--;
+//                  if(mp[nums[i]] == 0) mp.erase(nums[i]);
+//              }
+//              else{
+//                  mp[nums[i]]++;
+//              }
+//          }
+        
+//         vector<int> ans;
+        
+//         for(auto it:mp){
+//             ans.push_back(it.first);
+//         }
+//         return ans;
     }
 };
