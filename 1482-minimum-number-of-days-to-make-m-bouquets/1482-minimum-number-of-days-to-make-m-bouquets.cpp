6f77bd25dc1e3007706a8877e9c275ebaa1145a8
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-06-19 22:34:15 +0530

    Time: 129 ms (8.54%), Space: 68.9 MB (92.92%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,72 +1,50 @@
 class Solution {
 public:
     
-    bool isPossible(vector<int>&bloomDay,int mid, int m,int k){
-    
-        long long temp = 0;
+    bool isPossible(int &mid, vector<int>&b, int m,int &k){
         
-        for(int i=0; i<bloomDay.size(); i++){
-            if(bloomDay[i] - mid <= 0){
-                //means floower is bloomed
-                //we can use it
-                temp++;
+        for(int i=0; i<b.size(); i++){
+            int temp = k;
+            
+            while(temp != 0 && i<b.size()){
+                if(b[i] > mid) break;
+                i++;
+                temp--;
             }
-            else{
-                //means floower is not bloomed 
-                //and we have to move forward and adjacent floower is also break
-                temp = 0;
-            }
-            //agr temp == k h to hum ek bouquet bana skte h
-            if(temp == k){
+            
+            if(temp == 0){
+                i--;
                 m--;
-                //now to check for another k adjacent elements
-                temp = 0;
             }
+            
             if(m == 0) return true;
         }
         
-        //agr k == 0 iska mtlb h ki we can make bouquets
-        return m <= 0;
+        return false;
     }
     
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        //this is the kind of binary search problem where 
-        //ans is definetly between the min value of the bloomDay and maxValue of bloomDay
-        //we have search space here and apply binary search 
+    int minDays(vector<int>& bloomDay, int m, int k){
         
-        if(bloomDay.size()/k < m) return -1;
+         int e = *std::max_element(bloomDay.begin(), bloomDay.end());
+         int s = *std::min_element(bloomDay.begin(), bloomDay.end());
         
-        long long s = INT_MAX;
-        long long e = INT_MIN;
-        
-        for(int i=0; i<bloomDay.size(); i++){
-            if(s > bloomDay[i]){
-                s = bloomDay[i];
-            }
-            if(e < bloomDay[i]){
-                e = bloomDay[i];
-            }
-        }
-        
-        long long ans = 0;
+        int ans = -1;
         
         while(s<=e){
-            long long mid = s + (e-s)/2;
+            int mid = s + (e-s)/2;
             
-            bool check = isPossible(bloomDay,mid,m,k);
-            
-            if(check){
-                //than store the answer and check for better answer
+            if(isPossible(mid,bloomDay,m,k)){
+                //store the answer
                 ans = mid;
+                //check for most optimal answer
                 e = mid-1;
             }
             else{
-                //bouquets banana possible nhi h aur wait krna pdega
-                //means number of days will increase here
-                s = mid+1;   
+                s = mid+1;
             }
         }
-                
+        
         return ans;
+        
     }
 };
