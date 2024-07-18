d0e752265578d786198b77bd03e2e8039566f18c
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-07-18 23:18:29 +0530

    Time: 278 ms (6.19%), Space: 118.5 MB (5.24%) - LeetHub

---
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.cpp
@@ -0,0 +1,85 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    void makeGraph(TreeNode* root, TreeNode* prev,unordered_map<TreeNode*,vector<TreeNode*>> &graph,unordered_set<TreeNode*> &st){
+        
+        //base case
+        if(!root) return;
+        
+        //root node h to set m store kr lo
+        if(root->left == NULL && root->right == NULL){
+            st.insert(root); 
+        }
+        
+        //make a connection
+        if(prev != NULL){
+            graph[prev].push_back(root);
+            graph[root].push_back(prev);
+        }
+        
+        makeGraph(root->left,root,graph,st);
+        makeGraph(root->right,root,graph,st);
+    }
+    
+    int countPairs(TreeNode* root, int distance) {
+        //step1 -- make an undirected graph and store all the leaf nodes
+        //step2 -- calculate shortest distance of all <= distance
+        
+        //make undirected graph first
+        unordered_map<TreeNode*,vector<TreeNode*>> graph;
+        
+        unordered_set<TreeNode*> st;
+        
+        //need a prev node to establish connection
+        makeGraph(root,NULL,graph,st);
+        
+        int ans = 0;
+        
+        //now find distance
+        for(auto &it:st){
+            
+            queue<TreeNode*> q;
+            unordered_set<TreeNode*> visited;
+            q.push(it);
+            
+            visited.insert(it);
+            
+            
+            for(int level = 0; level <= distance; level++){ // at a time only go till level <= distance
+                
+                int size = q.size();
+                
+                while(size--){
+                    TreeNode* temp = q.front();
+                    q.pop();
+                    
+                    if(temp != it && st.count(temp)){ //means curr node temp leaf node h which is store in the set
+                        ans++;
+                    }
+                    
+                    for(auto & ngbr:graph[temp]){
+                        if(!visited.count(ngbr)){
+                            //means ngbr is not visited then push it into the q
+                            q.push(ngbr);
+                            visited.insert(ngbr);
+                        }
+                    }
+                }
+            }
+        }
+        
+        return ans/2;
+        
+    }
+};
