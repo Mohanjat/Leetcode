fb55f74902a5c809ba53f595582a33fd10605220
Mohanjat <97402247+Mohanjat@users.noreply.github.com>
2024-07-22 20:56:28 +0530

    Time: 23 ms (85.84%), Space: 26.8 MB (55.79%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        unordered_map<int,string> mp;
+        
+        for(int i=0; i<names.size(); i++){
+            mp[heights[i]] = names[i];
+        }
+        
+        sort(heights.begin(),heights.end(),greater<int>());
+        
+        vector<string> ans;
+        
+        for(int i=0; i<heights.size(); i++){
+             ans.push_back(mp[heights[i]]);
+        }
+        
+        return ans;
+    }
+};
